024c840ccbe9d9ff41481004b03ff1dd519c7af8
Vitalii <tka4uk.gt41@gmail.com>
2021-04-01 00:52:21 +0600

    @media

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 $(function () {
+    $(".photo__slider").slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/photo/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/photo/right.svg"></button>',
+    });
+
+    // кнопка навверх =============================
     
+    let btn = $('.pageUp');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 1000);
+    });
+
+    // =================================================
+    
+
+    $('.menu__btn').click( function () {
+        
+    })
+
 })
